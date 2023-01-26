d26c74145bb156918c4deed50a3509a015602865
GioGio4 <g.ceccobelli@outlook.it>
2023-01-26 15:28:45 +0100

    Add js code to (esercizio Mail), use array-cycle

--- Mail/js/main.js
+++ Mail/js/main.js
@@ -2,3 +2,42 @@
 // Chiedi all'utente la sua email,
 // controlla che sia nella lista di chi può accedere,
 // stampa un messaggio appropriato sull'esito del controllo.
+
+
+// 1- Ho bisogno di una lista che contenga le mail degli utenti
+const mail_list = [
+    "mail1",
+    "mail2",
+    "mail3",
+    "mail4"
+];
+
+// console.log("lista email=" + mail_list);
+
+// 2- Devo creare un prompt per far inserire la mail all'utente che accede
+const user_mail = prompt("Inserisci Email per accedere");
+
+
+// 3- SE La mail coincide con una nella lista viasualizza messaggio "benvenuto"
+//   ALTRIMENTI visualizza messsaggio "accesso negato"
+
+// Ho bisogno di uno switch, altrimenti in caso di condizione "accesso negato" lo ripeterà per ogni ciclo.
+let welcome_msg = false
+
+for (let i = 0; i < mail_list.length; i++) {
+
+    if (user_mail == current_element) {
+
+        welcome_msg = true
+    }
+
+}
+
+// Fuori dal ciclo altrimenti ripete ricerca e riporta alert ad ogni ciclo
+
+if (welcome_msg == true) {
+    alert("BENVENUTO")
+}
+else {
+    alert("ACCESSO NEGATO")
+}
